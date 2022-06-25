e7a936505159c40534dda0eb4426608e68a880b4
Allan <lanvonov@gmail.com>
2022-06-25 02:21:39 -0300

    Web update

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+// axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
+
 export const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
 })
